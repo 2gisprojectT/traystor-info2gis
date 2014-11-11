86aa1bde80fde34c1c206bcbe268090cffda9da3
Эльдар Трайстор <eldar.traistor@mail.ru>
2014-11-11 13:13:02 +0700

    Update unsubscribe.py

--- 5.HW_patterns(info.2gis.ru)/unsubscribe.py
+++ 5.HW_patterns(info.2gis.ru)/unsubscribe.py
@@ -5,7 +5,6 @@ class Unsubscribe(BaseComponent):
     selectors = {
         'self': '#module-1-7-1 > div > dd:nth-child(4)',
         'unsub': '//*[@id="module-1-7-1"]/div/dt[2]/span',
-        'clickBtnUnsubscribe': '#module-1-7-1-2 > form > div > input',
         'email': '#subscribeconfirmation_email',
         'resultE': '#module-1-7-1-2 > form > ul > li.form__item._error > span'
     }
@@ -17,8 +16,5 @@ class Unsubscribe(BaseComponent):
         self.driver.find_element_by_css_selector(self.selectors['email']).click()
         self.driver.find_element_by_css_selector(self.selectors['email']).send_keys(email)
 
-    def clickButton(self):
-        self.driver.find_element_by_css_selector(self.selectors['clickBtnUnsubscribe']).click()
-
-    def resultE(self):
+    def resultEmail(self):
         self.driver.find_element_by_css_selector(self.selectors['resultE'])
